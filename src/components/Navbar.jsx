6249849152a7b664e24713521ca59aab613fab30
Goph <tealeverywhere@gmail.com>
2023-12-20 21:46:26 -1000

    Added cart item badge

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,13 +3,21 @@ import "../styles/Navbar.css"
 
 function Navbar() {
 
+  const numCartItems = 1;
+
   return (
     <div className="navbar">
       <nav>
         <ul>
           <li className="linkWrapper">
             <Link to="home">Home</Link>
-            <Link to="cart">My Shopping Cart</Link>
+            <div className="navCart">
+              <Link to="cart">My Shopping Cart</Link>
+              {numCartItems ? 
+                <i data-count={numCartItems} className="badge"></i> :
+                <></>
+              }
+            </div>
           </li>
         </ul>
       </nav>
